56cc34443d61a88d9c2b6e533238d5338aea6919
Kim SoYeon <claudia825@dgu.ac.kr>
2021-11-06 22:21:20 +0900

    final

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
+from pandas.core.strings import copy
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 from konlpy.tag import Okt
 import pandas as pd
 import numpy as np
-from kiwipiepy import Kiwi, Option
+from kiwipiepy import Kiwi
 from gensim.models import FastText
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import fasttext
 import re
-import io
-import json
-import csv
-import openpyxl
 import pandas as pd
 import numpy as np
 import ast
@@ -25,9 +21,9 @@ from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
-from IPython.core.display import display, HTML
-from pykospacing import spacing
 import streamlit as st
+from matplotlib import font_manager, rc
+import platform
 
 
 def crawl(date, fin):
@@ -164,6 +160,7 @@ def newwords():
 
 def an(new):
     loaded_model = FastText.load("fasttext")
+    global Similar
     Similar = []
     similar_word_list = list(loaded_model.wv.most_similar(str(new), topn=10)) 
     for i in range(len(similar_word_list)):
@@ -171,18 +168,200 @@ def an(new):
         Similar.append(temp_str)
     
     st.write(Similar)
+    return Similar
 
+def extract_word(fkey,key):
+    
+    final = pd.DataFrame(columns = ['contents'])
+
+    f = pd.read_csv('natepann_data.csv')
+
+    is_key = (f['본문'].str.contains(fkey, na=False) | f['제목'].str.contains(fkey, na=False)) & (f['본문'].str.contains(key, na=False) | f['제목'].str.contains(key, na=False))
+
+    f_key = f[is_key]
+    
+    if f_key.empty:
+        pass
+    else: 
+        f_key = f_key.reset_index()
+
+        final_key = pd.DataFrame(columns = ['tokenized'])
+
+        komoran = Komoran()
+
+        for i in range(len(f_key)):
+            try:
+                st = f_key.loc[i, '제목']
+                final_key.loc[i, 'tokenized'] = str(st)
+                ct = f_key.loc[i, '본문']
+                final_key.loc[i, 'tokenized'] += str(ct)
+                
+            except KeyError:
+                print("error")
+
+        keyword_content = pd.DataFrame(columns = ['content','positive', 'negative', 'neutral'])
+
+        for i in range(len(final_key)):
+            try:
+                fc = final_key.loc[i, 'tokenized']
+                
+                if i == 0:
+                    keyword_content.loc[0,'content'] = fc
+                else:
+                    keyword_content.loc[0,'content'] += fc
+                
+            except KeyError:
+                print("error")
+
+        #st.dataframe(keyword_content, columns = ['content'])
+
+        # 감성사전에서 감성분석
+        f = open('SentiWord_Dict.txt', 'r', -1, 'utf-8')
+        lines = f.readlines()
+
+        score_dict = []
+
+        for line in lines:
+            line_splited = line.split()
+            score = int(line_splited[-1])
+            word = ''
+            for frac in line_splited[:-1]:
+                word = word + ' ' + frac
+            
+            word = word[1:]
+            score_dict.append([word, score])
+            
+        keyword_content['positive'] = 0
+        keyword_content['negative'] = 0
+        keyword_content['neutral'] = 0
+
+        if keyword_content.empty:
+            #print("사전과 비교할 조건에 맞는 내용 없음")
+            pass
+        else:
+            keyword_content.loc[0, 'content1'] = str(komoran.nouns(keyword_content.loc[0, 'content']))
+
+
+            stopwords = ['하다', '없다', '있다', '되다', '아니다', '같다', '이다', '않다', '그렇다', 
+                        '이렇다', '싶다', '다', '것', '그', '이', '거', '니다']
+
+
+            pos_list = ast.literal_eval(keyword_content.loc[0, 'content1'])
+
+            final = []
+
+            for j in range(len(pos_list)):
+                if pos_list[j] not in stopwords:
+                    final.append(pos_list[j])
+
+            keyword_content.loc[0, 'content1'] = str(final)
+
+            # 감성사전에서 감성분석
+            f = open('SentiWord_Dict.txt', 'r', -1, 'utf-8')
+            lines = f.readlines()
+
+            score_dict = []
+
+            for line in lines:
+                line_splited = line.split()
+                score = int(line_splited[-1])
+                word = ''
+                for frac in line_splited[:-1]:
+                    word = word + ' ' + frac
+
+                    word = word[1:]
+                    score_dict.append([word, score])
+
+            keyword_content.insert(0, "Keyword", key)
+
+            tokens = ast.literal_eval(keyword_content.loc[0, 'content1'])
+
+            try:
+                for token in tokens:
+                    for dict_word in score_dict:
+                        if dict_word[0] == token:
+                            if dict_word[1] > 0:
+                                keyword_content.loc[0, 'positive'] += dict_word[1]
+                            elif dict_word[1] < 0:
+                                keyword_content.loc[0, 'negative'] += dict_word[1]
+                            else:
+                                keyword_content.loc[0, 'neutral'] += 1 #중립어는 개수 세기    
+            except KeyError:
+                print("error")
+        
+        #keyword_content[keyword_content]
+        df_reset=keyword_content.set_index('Keyword')
+        df_reset[df_reset.columns.difference(['content1'])]
+
+        global pos, neg, neu
+        pos.append(int(keyword_content.loc[0,'positive']))
+        neg.append(int(keyword_content.loc[0,'negative']))
+        neu.append(int(keyword_content.loc[0,'neutral']))
+
+
+def NLP(a,b,c) :
+    font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+    rc('font', family=font_name)
+
+    pos = a
+    neg = b
+    nneg = str(neg).replace("-", "")
+    neu = c 
+    
+    p = pos / (pos + int(nneg))*100
+    n = int(nneg) / (pos + int(nneg))*100
+    total = p - n
+
+    if total <= 10 and total >= -10:
+        Senti = 0
+    elif total < -10 and total >= -50:
+        Senti = -1
+    elif total < -50:
+        Senti = -2
+    elif total > 10 and total<= 50:
+        Senti = 1
+    elif total > 50:
+        Senti = 2
+
+    size = [pos, nneg, neu]
+
+    label = ['Positive','Negative', 'Neutral']
+    colors = ['#FBF9A9', '#FFD9D9', '#8fd9b6']
+
+    plt.subplots()
+    plt.axis('equal')
+    plt.rc('font', size=10)
+    plt.pie(x=size, startangle=200, colors = colors, shadow = True, labels=label, autopct='%.2f%%')
+    plt.rc('legend', fontsize=8)
+    plt.legend(loc="upper right")
+
+    plt.title(str(fkey) + "의 극성값 = " + str(Senti))
+    #plt.show()
+    st.pyplot(plt)
 
 
 if __name__ == '__main__':
 
-    st.header("온라인 커뮤니티 특화 감성 사전 구축을 위한 새로운 용어 극성값 분석 시스템")
+    #footer 수정
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden; }
+                footer {visibility: hidden;}
+                footer:after {visibility: visible; content:"BigDataTerminator";}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+   
+    st.title("신조어 극성값 분석 시스템")
 
     st.subheader("BigDataTerminator")
+    #st.subheader("온라인 커뮤니티 특화 감성 사전 구축을 위한 신조어의 극성값 분석 시스템")
 
     st.write("데이터 크롤링을 시작할 날짜를 입력해주세요.")
-    st.write("TEST) 15일치 데이터를 크롤링합니다.")
-    date = st.text_input("시작 날짜 입력. (ex. 20210920) : ")
+    st.write("6개월 데이터를 크롤링합니다.")
+    date = st.text_input("시작 날짜 입력. (ex. 20210920) : ", help=None)
+
     # fin = 0
 
     #웹에서 바로 크롤링하면 2일치만 해도 너무 느림... 
@@ -195,16 +374,35 @@ if __name__ == '__main__':
 
     #     st.subheader("연관 용어 추출")
 
+    pos = []
+    neg = []
+    neu = []
+    global key, fkey
 
+    
     if len(date) >= 8:
-        st.write("크롤링 완료")
-
-        st.subheader("새로운 용어 추출")
+        st.write('크롤링 완료!')
+        st.subheader("신조어 추출")
         newwords()
 
-        new = st.text_input("""분석할 "새로운 용어"를 입력하세요. : """)
-        
-        if len(new) >= 1:
-            st.subheader("'" + new + "' 의 연관 용어 추출")
+        new = st.text_input("""분석할 "신조어"를 입력하세요. : """)
 
+        if st.button('신조어 분석'):
+            fkey = new
+            st.subheader("'" + new + "' 의 연관 용어 추출")
             an(new)
+            k = 0
+            k += 1
+            
+            if k > 0:
+                st.subheader("신조어 : '" + new +  "' 의 감성 분석")
+                with st.spinner("연관용어 분석 중..."):
+                    for i in range(len(Similar)): 
+                        key = Similar[i]
+                        extract_word(fkey,key)
+                st.success("분석 완료")
+
+                st.write(" ")
+                st.subheader("분석 결과 :")
+                NLP(sum(pos),sum(neg),sum(neu))
+            
